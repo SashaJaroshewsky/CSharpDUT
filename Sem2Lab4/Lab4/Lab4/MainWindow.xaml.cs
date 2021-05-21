499036dbfe64993f9c7624a791c7fa4658e2d692
Sasha_Jaroshewsky <59161237+SashaJaroshewsky@users.noreply.github.com>
2021-05-21 23:23:54 +0300

    Add files via upload

---
+++ Sem2Lab4/Lab4/Lab4/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (UIElement el in MainGrid.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+
+            if (str == "C")
+            {
+                label.Text = "";
+            }else if (str == "=")
+            {
+                string result = new DataTable().Compute(label.Text, null).ToString();
+                label.Text = result;
+            }
+            else
+            {
+                label.Text += str;
+            }
+
+        }
+    }
+}
